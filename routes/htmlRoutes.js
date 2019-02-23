50eb579777a9a19a2bca094f6918ca2a1920727e
fkakatie <sextonskatie@gmail.com>
2019-02-23 12:25:09 -0700

    handlebars frameworks and html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,34 +1,33 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  app.get("/", function (req, res) {
+    res.render("index");
   });
 
-  // Load index page
-  app.get("/user", function(req, res) {
-      res.render("user", {
-        examples: dbExamples
-      });
+  // Load USER page
+  app.get("/user", function (req, res) {
+    res.render("user");
+  });
+
+  // Load GROUP page
+  app.get("/group", function (req, res) {
+    res.render("group");
+  });
+
+  // Load EVENT page
+  app.get("/event", function (req, res) {
+    res.render("event");
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/example/:id", function (req, res) {
+    res.render("example");
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
