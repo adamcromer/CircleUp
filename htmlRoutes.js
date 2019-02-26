cc79c2c56a8b42f36f61809e5ad8b550f53b1ee2
adamjorycromer <adamjorycromer@gmail.com>
2019-02-25 19:14:50 -0700

    pushing to heroku

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,35 +1,34 @@
-var db = require("../models");
+var db = require("./models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     res.render("index");
   });
 
   // Load USER page
-  app.get("/user", function (req, res) {
+  app.get("/user", function(req, res) {
     res.render("user");
   });
 
   // Load GROUP page
-  app.get("/group", function (req, res) {
+  app.get("/group", function(req, res) {
     res.render("group");
   });
 
   // Load EVENT page
-  app.get("/event", function (req, res) {
+  app.get("/event", function(req, res) {
     res.render("event");
   });
 
   // Load example page and pass in an example by id
 
-  app.get("/example/:id", function (req, res) {
+  app.get("/example/:id", function(req, res) {
     res.render("example");
-
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
