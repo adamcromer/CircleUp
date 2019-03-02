6f7841a28ede964c5d6c980e8f00742eff153921
adamjorycromer <adamjorycromer@gmail.com>
2019-03-02 10:32:53 -0700

    working event api test file

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  
   // Getting references to our form and input
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
@@ -8,12 +7,11 @@ $(document).ready(function() {
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
-    
+
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim(),
       name: nameInput.val().trim()
-   
     };
     console.log(userData);
     if (!userData.email || !userData.password || !userData.name) {
@@ -33,19 +31,21 @@ $(document).ready(function() {
       email: email,
       password: password,
       name: name
-    }).then(function(data) {
-      console.log("after signup api call", data.errors[0].message);
-      if (data.errors[0].message === "email must be unique"){
-        $("#error").text("Email is already taken!");
-        return;
-      } 
-      window.location.replace(data);
-      // If there's an error, handle it by throwing up a bootstrap alert
-    }).catch(handleLoginErr);
+    })
+      .then(function(data) {
+        console.log("after signup api call", data.errors[0].message);
+        if (data.errors[0].message === "email must be unique") {
+          $("#error").text("Email is already taken!");
+          return;
+        }
+        window.location.replace(data);
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
   }
 
   function handleLoginErr(err) {
-    console.log("helloFrontend")
+    console.log("helloFrontend");
     $("#alert .msg").text(err.responseJSON);
     $("#alert").fadeIn(500);
   }
