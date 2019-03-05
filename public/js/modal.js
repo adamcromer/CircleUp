2d6562ca604b43480a2d2d07f1a5bb040080aade
adamjorycromer <adamjorycromer@gmail.com>
2019-03-04 17:49:49 -0700

    working on creating event

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // Get the modal
 var modalJoin = $("#modal-join");
 var modalCreate = $("#modal-create");
@@ -6,7 +7,7 @@ var modalCreate = $("#modal-create");
 var btnJoin = $("#btn-join");
 var btnCreate = $("#btn-create");
 
-// When the user clicks on the button, open the modal//
+// When the user clicks on the button, open the modal
 btnJoin.click(function() {
   modalJoin.css("display", "block");
 });
@@ -38,3 +39,66 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".timepicker").timepicker();
 });
+
+var createEvent = $("#create-event");
+
+//Simplified array to string function
+function arrayToString(arr) {
+  var arrString = arr.join(", ");
+  return arrString;
+}
+
+//Simplified string to array function
+function stringToArray(str) {
+  var stringArr = str.split(" ");
+  return stringArr;
+}
+
+//When user clicks create, data gets posted and hides the modal
+createEvent.click(function() {
+  var name = $("#new-event-name-input")
+    .val()
+    .trim();
+  var date = $("#new-event-date-input")
+    .val()
+    .trim();
+  var time = $("#new-event-time-input")
+    .val()
+    .trim();
+  var location = $("#address")
+    .val()
+    .trim();
+  var description = $("#new-event-description-input")
+    .val()
+    .trim();
+  var itemsToBring =
+    $("#item_needed1")
+      .val()
+      .trim() +
+    ", " +
+    $("#item_needed2")
+      .val()
+      .trim() +
+    ", " +
+    $("#item_needed3")
+      .val()
+      .trim() +
+    ", " +
+    $("#item_needed4")
+      .val()
+      .trim();
+  var event = {
+    name: name,
+    dateAndTime: date + " " + time,
+    location: location,
+    description: description,
+    itemsToBring: itemsToBring
+  };
+  postEvent(event);
+  console.log(event);
+  console.log(date, time);
+});
+
+function postEvent(eventData) {
+  $.post("/api/events", eventData).then(modalCreate.css("display", "none"));
+}
